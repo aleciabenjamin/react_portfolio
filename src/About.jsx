5095b1ebc59edcfad26dae6616f6a6b2171f6094
Alecia <aleciabenjamin@gmail.com>
2019-02-09 21:42:40 +0100

    create buttons for previous employment and education

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,18 @@ const About = () => {
                         <p className="content-text">“People find me to be an upbeat, self-motivated team player with excellent communication skills.
                         For the past several years I have worked in lead qualification, telemarketing, and customer service in the technology industry. My experience includes successfully calling people in director-level positions of technology departments and developing viable leads. I have a track record of maintaining a consistent call and activity volume and consistently achieving the top 10 percent in sales, and I can do the same thing for your company.”</p>
                     </div>
-
+                    <div className="flex items-center justify-between leading-none p-2 md:p-4">
+                
+                    <button className="bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded">
+                        Previous Employment 
+                        </button>
+                        <button className="bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded">
+                        Education 
+                        </button>
+                    
+                    
+                
+                    </div>
                 </div>
              </div>
     );
