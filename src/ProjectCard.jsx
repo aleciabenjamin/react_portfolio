23442f1c310e083e5b4c41fd2990a96003544217
Alecia <aleciabenjamin@gmail.com>
2019-02-09 20:29:44 +0100

    create and populate jobs.json and education.json

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -6,26 +6,27 @@ const ProjectCard = (props) => {
 
         <div key={project.id} className="overflow-hidden border">
             <a href={project.url} target="_blank">
-                
-                
             <img src={project.image} className="block h-auto w-full" style={{ height: '250px', objectFit: 'cover' }} />
             </a>
 
             <div className="px-6 py-4" style={{ minHeight: '150px' }} >
                 <div className="font-bold text-2xl text-grey-darker mb-2">{project.name}</div>
-                <div>             
-                    
+                    <div>             
                     </div>
-                <p className="text-grey-darker text-base">
-                    {project.description}
-                </p>
-            </div>
+                            <p className="text-grey-darker text-base">
+                                {project.description}
+                            </p>
+                </div>
 
             <div className="flex items-center justify-between leading-none p-2 md:p-4">
-            
-                <button className="bg-grey-darkest hover:bg-blue-dark text-white text-xs py-1 px-2 rounded-full">
-                    Details
-                </button>
+                <a href={project.url} target="_blank">
+                    <button className="bg-grey-darkest hover:bg-blue-dark text-white text-xs py-1 px-2 rounded-full">
+                        Go to 
+                    
+                    </button>
+                </a>
+                
+                
                 
                 
             </div>
